4a38d95ec201f732abb257e5466a873a759f1980
JurgenGalicia <jurgen.galicia@gmail.com>
2026-04-23 00:42:08 -0700

    added more components and incorporated bootstrap

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">item 1</li>
+      <li className="list-group-item">item 2</li>
+      <li className="list-group-item">item 3</li>
+      <li className="list-group-item">item 4</li>
+      <li className="list-group-item">item 5</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
