d3f704cca658f804409e294198bedd64c97a818c
JurgenGalicia <jurgen.galicia@gmail.com>
2026-04-24 09:55:55 -0700

    learning about useState and state management

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,18 @@
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //   let selectedIndex = 0;
+  //hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //   const [name, setName] = useState("");
+  // arr[0] variable (selectedIndex)
+  // arr[1] updater function
 
   // eventHandler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //   const handleClick = (event: MouseEvent) => (selectedIndex = this.index);
 
   return (
     <>
@@ -12,7 +20,17 @@ function ListGroup() {
       {items.length === 0 && <p>no items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li key={item} onClick={handleClick} className="list-group-item">
+          <li
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
             {item}
           </li>
         ))}
