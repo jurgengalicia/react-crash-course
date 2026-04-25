4d98c7bca183cd15f07f01d4fd4fa0390765902e
JurgenGalicia <jurgen.galicia@gmail.com>
2026-04-24 17:49:51 -0700

    alert add and remove exercise

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,19 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface AlertProps {
   children: ReactNode;
+  state: string;
+  handleXClick: () => void;
 }
 
-const Alert = ({ children }: AlertProps) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, state, handleXClick }: AlertProps) => {
+  // const [currentState, setState] = useState("fade");
+  return (
+    <div className={`alert alert-warning alert-dismissible`}>
+      {children}
+      <button onClick={handleXClick} className="btn-close"></button>
+    </div>
+  );
 };
 
 export default Alert;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,20 +3,26 @@ import React, { useState, ReactNode } from "react";
 interface ButtonProp {
   children: ReactNode;
   buttonNames: string[];
+  onBtnClick: () => void;
 }
 
 export default function Button({
   children,
-
   buttonNames,
+  onBtnClick,
 }: ButtonProp) {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const handleClick = () => {
-    setSelectedIndex((prev) => (prev + 1) % buttonNames.length);
-  };
+  //   const handleClick = () => {
+  //     setSelectedIndex((prev) => (prev + 1) % buttonNames.length);
+  //   };
+  const incrementButton = (prev: number) => (prev + 1) % buttonNames.length;
   return (
     <button
-      onClick={handleClick}
+      onClick={() => {
+        setSelectedIndex(incrementButton);
+        onBtnClick();
+        console.log(selectedIndex);
+      }}
       className={`btn btn-${buttonNames[selectedIndex]}`}
     >
       {children}
